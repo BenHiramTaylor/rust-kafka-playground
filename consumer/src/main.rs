750526d185b840012460ec8bd44849ce6772d2f0
Ben Taylor <63203762+BenHiramTaylor@users.noreply.github.com>
2024-07-11 15:56:55 +0100

    feat: added binary to consume async messages from topic

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use log::{error, info};
+use rdkafka::ClientConfig;
+use rdkafka::consumer::{Consumer, StreamConsumer};
+use rdkafka::Message;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    env_logger::init();
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("group.id", "name_consumer_group")
+        .set("bootstrap.servers", "kafka:9092")
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .create()?;
+
+    consumer.subscribe(&["names_topic"])?;
+
+    info!("Started consuming");
+
+    loop {
+        match consumer.recv().await {
+            Ok(message) => {
+                if let Some(name) = message.payload_view::<str>() {
+                    match name {
+                        Ok(name) => println!("Hello {}", name),
+                        Err(e) => error!("Error parsing message payload: {:?}", e),
+                    }
+                }
+            }
+            Err(e) => error!("Error while receiving message: {:?}", e),
+        }
+    }
 }
